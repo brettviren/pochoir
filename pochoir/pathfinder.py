e53348c60430e90dc0653cca7c599ea92ebd8205
Sergey Martynenko <sergey@Sergeys-MBP.home>
2021-03-31 15:32:50 -0400

    Scipy versioon of RK redesign
    The updated version of the RK ODE solver for the electron drift in the electric field. The design is made similar to the torch version.

--- pochoir/pathfinder.py
+++ pochoir/pathfinder.py
@@ -4,92 +4,53 @@ Solve electron motion in Efiled with scipy RK method
 '''
 
 import numpy as np
-from scipy.integrate import ode
+from .arrays import to_numpy
+from scipy.integrate import solve_ivp as ode
+from scipy.integrate import odeint
+from scipy.interpolate import RegularGridInterpolator as RGI
 
-
-def E_vect(x,efield):
-    """
-    Take the closest E filed o the grid to considered particle position
-    """
-    shape = (84,56,1000)
-    dim = (2.5,1.67,30)
-    i=int(x[0]*shape[0]/dim[0])-1
-    j=int(x[1]*shape[1]/dim[1])-1
-    k=int(x[2]*shape[2]/dim[2])-1
-    print(i,j,k,efield[0][i,j,k],efield[1][i,j,k],efield[2][i,j,k])
-    return efield[0][i,j,k],efield[1][i,j,k],efield[2][i,j,k]
-
-def inBound(x,boundary):
-    """
-    Check if point is in or out of boundary
-    """
-    shape = (84,56,1000)
-    dim = (2.5,1.67,30)
-    i=int(x[0]*shape[0]/dim[0])-1
-    j=int(x[1]*shape[1]/dim[1])-1
-    k=int(x[2]*shape[2]/dim[2])-1
-    indexarr = (i,j,k)
-    if any(v<0 for v in indexarr):
-        return 1
-    if boundary[i,j,k]==0:
-        return 0
-    return 1
-
-
-def e_of_x(x):
-    return 10 * np.sign(np.sin(2 * np.pi * x / 25))
-
-def deriv(t, Y, q, m, B, E):
-    """Derivative of the state vector y according to the equation of motion:
-    Y is the state vector (x, y, z, u, v, w) === (position, velocity).
-    returns dY/dt.
-    """
-    x, y, z = Y[0], Y[1], Y[2]
-    u, v, w = Y[3], Y[4], Y[5]
+class Simple:
+    '''
+    Simple ODE calable
+    '''
     
-    alpha = q / m * B
-    return np.array([u, v, w, E[0], alpha * w + E[1], -alpha * v+E[2]])
+    def __init__(self, domain, vfield):
+        '''
+        The vfield give vector feild on domain.
+        '''
+        points = list()
+        for num, spacing, origin in zip(domain.shape, domain.spacing, domain.origin):
+            start = origin
+            stop  = origin + num * spacing
+            points.append(np.arange(start, stop, spacing))
+        self.interp =[RGI(points, coord) for coord in vfield]
 
-def calc_traj_scipy(m,q,t0,t1,dt,efield,boundary,initial_conditions):
-    r = ode(deriv).set_integrator('dopri5')
-    r.set_initial_value(initial_conditions, t0).set_f_params(m, q, 0.0, 10.)
-    positions = []
-    velocities = []
-    while r.successful() and r.t < t1:
-        r.set_f_params(m, q, 0.0, E_vect(r.y[:3],efield))
-        r.integrate(r.t+dt)
-        positions.append(r.y[:3])
-        velocities.append(r.y[3:6])
-        if inBound(r.y[:3],boundary)==1:
-            break
-    return np.array(positions),np.array(velocities)
+    def __call__(self, tick, tpoint):
+        '''
+        Return velocity vector at location (time independent).
 
+        Location is given as an index (but floating point) vector.
+        '''
+        velo = np.zeros_like(tpoint)
 
-def solve(ipos,ivel,barr,efield,time,step):
+        point = np.array(tpoint)#[np.array([t]) for t in tpoint]
+        
+        for ind, inter in enumerate(self.interp):
 
+            velo[ind] = inter(point)[0]
+        
+        return velo
 
-    t0=0.0
-    
-    positions = []
-    velocities = []
-    print(ipos,ivel)
-    for ip in ipos:
-        print(ip)
-    for ip,iv in zip(ipos,ivel):
-        p,v=calc_traj_scipy(1.0, 1.0,t0,time,step,efield,barr,np.concatenate((ip, iv)))
-        positions.append(p)
-        velocities.append(v)
-    #Debug Plots
-   # import matplotlib as mpl
-   # import matplotlib.pyplot as plt
-   # from mpl_toolkits.mplot3d import Axes3D
-   # fig = plt.figure()
-   # ax = fig.add_subplot(111, projection='3d')
-   # for position in positions:
-   #     ax.plot3D(position[:, 0], position[:, 1], position[:, 2])
-   # plt.xlabel('x')
-   # plt.ylabel('y')
-   # ax.set_zlabel('z')
-   # plt.show()
-    return positions,velocities
+def solve(domain,start,velocity,times):
+    '''
+    Return the path of points at times from start through velocity field.
+    Using SciPy RK method
+    '''
+    start = to_numpy(start)
+    velocity = to_numpy(velocity)
+    times = to_numpy(times)
+    print(times.shape)
+    func = Simple(domain, velocity)
+    return odeint(func,start,times,tfirst=True)
+
 
