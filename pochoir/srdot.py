b6069781ffc8c66e3f221f40fda6f7b6f17007e7
Sergey Martynenko <martynenko1sv@gmail.com>
2021-05-08 10:09:35 -0400

    Bug Fix
    Return current for many paths separately

--- pochoir/srdot.py
+++ pochoir/srdot.py
@@ -27,6 +27,7 @@ def dotprod(pcb_3Dstrips_domain,pcb_domain,pcb_3Dstrips_sol,pcb_drift,velo):
     I = []
     #This is slow, my attempt to make it interpolate for all points in path at once did not work (now it is about 13s per 10000 time ticks on mac)
     for path in pcb_drift:
+        I_i = []
         for point in path:
             #V is calcualted in quarter domain as drift is defined
             V = numpy.array([velo_interp[0](point)[0],velo_interp[1](point)[0],velo_interp[2](point)[0]])
@@ -35,6 +36,7 @@ def dotprod(pcb_3Dstrips_domain,pcb_domain,pcb_3Dstrips_sol,pcb_drift,velo):
             point[0]=point[0]+shift
             E = numpy.array([ew_interp[0](point)[0],ew_interp[1](point)[0],ew_interp[2](point)[0]])
             i = q*numpy.dot(E,V)
-            I.append(i)
+            I_i.append(i)
+        I.append(I_i)
 
     return I
